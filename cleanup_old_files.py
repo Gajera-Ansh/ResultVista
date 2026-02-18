3d925838b71ef0a4500a5d11ecc07964512c578e
Gajera-Ansh <gajeraansh4227@gmail.com>
2026-02-18 18:50:20 +0530

    if folder is not found than make a folder

--- cleanup_old_files.py
+++ cleanup_old_files.py
@@ -2,6 +2,10 @@ import os, time
 
 def cleanup_old_files():
     folder = "uploads"
+    # If folder is not exist
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
     for f in os.listdir(folder):
         path = os.path.join(folder, f)
         if os.path.isfile(path) and f.endswith((".xlsx", ".xls")):
