521dc1bc854043a77c7d8a2a39479f7d961e684e
Gajera-Ansh <gajeraansh4227@gmail.com>
2026-02-21 16:48:50 +0530

    Rename User class to Users for consistency with naming conventions

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(100), nullable=False)
