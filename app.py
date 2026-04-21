5a8ee33cbcf5f92e357e39097876612a7b50c58e
Gajera-Ansh <gajeraansh4227@gmail.com>
2026-04-21 19:52:42 +0530

    Update SQLAlchemy database URI to use environment variable

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/ResultVista"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 bcrypt = Bcrypt(app)
 
